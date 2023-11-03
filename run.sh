480c2ded98ea66cc453fa075f4bc364d15615c55
foonlife <hoge.life@gmail.com>
2023-11-03 11:57:35 +0900

    .

--- run.sh
+++ run.sh
@@ -4,52 +4,74 @@ source .bask.sh
 
 bask_default_task="usage"
 
-export DOCFX_VERSION=2.51
+export DOCFX_ARCH_ALL=amd64 arm64
+export DOCFX_VERSIONS=2.51
+export DOCFX_VERSION_LATEST=2.51
 
 task_usage() {
         bask_list_tasks
 }
 
 task_build() {
-        docker build . \
-                -t foonsoft/docker-docfx \
-                --build-arg DOCFX_VERSION
-}
-
-task_amd64() {
-        docker build . \
-                -t foonsoft/docker-docfx:${DOCFX_VERSION}.amd64 \
-                --build-arg DOCFX_VERSION \
-                --build-arg BUILDARCH=amd64
-
-        docker tag \
-                foonsoft/docker-docfx:${DOCFX_VERSION}.amd64 \
-                foonsoft/docker-docfx:amd64
-
-        docker tag \
-                foonsoft/docker-docfx:amd64 \
-                foonsoft/docker-docfx
-}
-
-task_arm64() {
-        docker build . \
-                -t foonsoft/docker-docfx:${DOCFX_VERSION}.arm64 \
-                --build-arg DOCFX_VERSION \
-                --build-arg BUILDARCH=arm64
-
-        docker tag \
-                foonsoft/docker-docfx:${DOCFX_VERSION}.arm64 \
-                foonsoft/docker-docfx:arm64
+        case ${OSTYPE} in
+        darwin*)
+                bask_run build ${DOCFX_LATEST_VERSION} arm64
+                bask_run tag ${DOCFX_LATEST_VERSION} arm64 latest
+                ;;
+        *)
+                bask_run build ${DOCFX_LATEST_VERSION} amd64
+                bask_run tag ${DOCFX_LATEST_VERSION} amd64 latest
+                ;;
+        esac
 }
 
 task_publish() {
-        bask_sequence amd64 arm64
+        docker rmi --force $(docker images -q foonsoft/docker-docfx) || :
+        bask_sequence all
+        docker push --all-tags foonsoft/docker-fx
+}
 
-        docker push \
-                foonsoft/docfx-docfx
-                foonsoft/docfx-docfx:amd64
-                foonsoft/docfx-docfx:${DOCFX_VERSION}.amd64
-                foonsoft/docfx-docfx:arm64
-                foonsoft/docfx-docfx:${DOCFX_VERSION}.arm64
+task_all() {
+        bask_sequence ${DOCFX_ARCH_ALL}
+        bask_run tag ${DOCFX_VERSION_LATEST} amd64 latest
+}
+
+task_amd64() {
+        bask_run build_versions ${DOCFX_VERSIONS} amd64
+        bask_run tag ${DOCFX_VERSION_LATEST} amd64 amd64
+}
+
+task_arm64() {
+        bask_run build_versions ${DOCFX_VERSIONS} arm64
+        bask_run tag ${DOCFX_VERSION_LATEST} arm64 arm64
+}
+
+build_versions() {
+        local versions=${1}
+        local arch=${2}
+
+        for v in ${versions} ; do
+                bask_run build ${v} ${arch}
+        done
+}
+
+build() {
+        local version=${1}
+        local arch=${2}
+
+        docker build . \
+                -t foonsoft/docker-docfx:${version}.${arch} \
+                --build-arg DOCFX_VERSION=${version} \
+                --build-arg BUILDARCH=${arch}
+}
+
+tag() {
+        local version=${1}
+        local arch=${2}
+        local new_tag=${3}
+
+        docker tag \
+                foonsoft/docker-docfx:${version}.${arch} \
+                foonsoft/docker-docfx${new_tag:-:}${new_tag}
 }
 
