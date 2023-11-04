82e18aacbbe3730679c7dd25d7b081334f0ed21b
foonlife <hoge.life@gmail.com>
2023-11-04 09:47:20 +0900

    add Sample Unity files.

---
+++ DocfxSample/src/Assets/DocfxSample/Sample.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+/// <summary>
+/// サンプルクラス
+/// </summary>
+public class Sample : MonoBehaviour
+{
+    /// <summary>
+    /// Start.
+    /// </summary>
+    void Start()
+    {
+        //
+    }
+}
